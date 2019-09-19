31e42ccc10655303331ffdfe9f3a7e6c3e2400e7
Saarthak Gupta <saarthak.gupta@chicagobooth.edu>
2019-09-19 20:45:52 +0000

    updated all functions, nothing works

--- linear_regression.py
+++ linear_regression.py
@@ -45,9 +45,15 @@ def compare_models(X, y):
         results (pandas.DataFrame) of estimated beta coefficients
     """
     
+    regression1 = LinearRegression(fit_intercept = False).fit(X,y)
+    regression2 = sm.OLS(y,X).fit()
     
+    reg1_coef = np.array(regression1.coef_)
+    reg2_coef = np.array(regression2.params)
 
-
+    beta_coefs_df = pd.DataFrame({"coef_1": reg1_coef, "coef_2": reg2_coef})
+    
+    return(beta_coefs_df)
 
 def load_hospital_data():
     """
@@ -58,12 +64,21 @@ def load_hospital_data():
         clean_df (pandas.DataFrame) containing the cleaned and formatted dataset for regression
     """
     data_dir = os.getcwd()
-    HospitalData = pd.readcsv(data_dir + "/hospital_charge_sample.csv")
+    HospitalData = pd.read_csv(data_dir + "/hospital_charge_sample.csv")
     
     return(HospitalData)
 
 
-def prepare_data():
+def prepare_data(df):
+    
+    subset = df.loc[:,("Provider State","Average Medicare Payments")]
+    
+    X = df["Provider State"]
+    y = df["Average Medicare Payments"]
+    
+    df_out = {"X" : X,
+              "y" : y}
+    
     """
     Prepares hospital data for regression (basically turns df into X and y).
     INPUT
@@ -71,10 +86,21 @@ def prepare_data():
     RETURNS
         data (dict) containing X design matrix and y response variable
     """
-    pass
+    
+    return(df_out)
 
 
 def run_hospital_regression():
+    
+    hospital_data = load_hospital_data()
+    hospital_clean = prepare_data(hospital_data)
+    
+    hospital_regress = sm.OLS(hospital_clean["y"],hospital_clean["X"]).fit()
+    
+    hospital_out = np.array(hospital_regress.params)
+    
+    return(hospital_out)
+    
     """
     Loads hospital charge data and runs OLS on it.
     INPUT
